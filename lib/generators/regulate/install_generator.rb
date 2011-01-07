f9a6749d55794267eded9f7c081af9d1470c7e5d
Ryan Cook <ryan@quickleft.com>
2011-01-07 10:57:29 -0700

    begin adding generators

---
+++ lib/generators/regulate/install_generator.rb
@@ -0,0 +1,32 @@
+module Regulate
+
+  module Generators
+
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Copies files to your application that regulate needs."
+
+      def copy_css
+        template "regulate.css", "public/stylesheets/regulate.css"
+      end
+
+      def copy_js
+        template "regulate.js", "public/javascripts/regulate.js"
+      end
+
+      def copy_yml
+        template "regulate.yml", "config/regulate.yml"
+      end
+
+      def copy_initializer
+        template "regulate.rb", "config/initializers/regulate.rb"
+      end
+
+    end # class InstallGenerator
+
+  end # Generators
+
+end # module Regulate
+
---
+++ lib/generators/templates/regulate.rb
@@ -0,0 +1,13 @@
+Regulate.setup do |config|
+
+  # Role that allows a user to manage pages and define what parts are editable, default is :admin
+  # The namespace that you would like to use for the routes. The default is 'cms'.
+  # @example Setting a custom route namespace
+  #   Regulate.setup do |config|
+  #     config.route_namespace = 'my_custom_route_namespace'
+  #   end
+  #
+  # config.route_namespace = 'my_custom_route_namespace'
+
+end
+
