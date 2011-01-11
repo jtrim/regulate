960c9e22f891cbda4fafcbf5134ecc76e5fb67b5
Collin Schaafsma <collin@quickleft.com>
2011-01-10 17:59:57 -0700

    js tweaks and documentation and some css defaults

--- public/javascripts/regulate_admin.js
+++ public/javascripts/regulate_admin.js
@@ -1,80 +1,120 @@
 (function( $ ) {
 
-  $.extend(jQuery.expr[':'], {
-    focus: function(element) {
-        return element == document.activeElement;
-    }
-  });
-
   var timer,
       stored_keys = null,
 
   Regulate = {
 
     longPoll: function longPoll( force, callback ){
-      var text = $('#page_view').val(),
-          m = /\{\{(\w)*\}\}/g,
+      var i, text = $('#page_view').val(),
+
+          // This RegEx will catch anything inside the `{{ }}`
+          m = /\{\{(\w|\s)*\}\}/g,
+          // A more generic mustache `{ }` grab
+          clean_key = /[\{,\}]/g,
+
           keys = [],
           invalid_keys = [],
           deleted_keys = [],
+
+          // To import data, we've attached an object to `window.existing_custom_fields`
           existing_keys = existing_custom_fields || {},
+
+          // Define a blacklist of keys we know we'll never want
           blacklist = ['{{view}}', '{{title}}'],
-          i,
+
+          // This assigns our initial match of all keys in the textarea
+          initial_match = text.match( m ) || [],
+
+          // This allows to skip the early return
           f = force || false;
 
-      if( $('#page_view').is(':focus') && f === false) {
+      // Prevent this core of this from running while someone's typing
+      if( $('#page_view').hasClass('active') && f === false) {
+        // Set the timer agains and return early
         timer = setTimeout( longPoll , 1e3);
         return false;
       }
 
-      $.each( text.match( m ), function( i, v ) {
-          if( ~blacklist.indexOf( v ) ) {
+      // Loop through the keys that our regex grabbed, and determine if they're ok to use
+      $.each( initial_match, function( i, v ) {
+          var invalid = false;
 
-            if( !~invalid_keys.indexOf( v ) ) {
-              invalid_keys.push( v );
-            }
+          // Keys can't be in the blacklist, also don't double up the errors
+          if( ~blacklist.indexOf( v ) && !~invalid_keys.indexOf( view ) ) {
+            invalid = true;
+            invalid_keys.push({ 'key':v, 'msg': 'The key '+v+' is reserved.' });
+          }
 
-          } else {
+          // Keys can't contain spaces
+          if( /\s/.test(v) && !~invalid_keys.indexOf( v ) ) {
+            invalid = true;
+            invalid_keys.push({ 'key':v, 'msg': 'The key '+v+' contains a space. It can\'t do that.' });
+          }
+
+          // Keys can't start with a number
+          if( /\{\{\d/.test(v) && !~invalid_keys.indexOf( v ) ) {
+            invalid = true;
+            invalid_keys.push({ 'key':v, 'msg': 'The key '+v+' starts with a number. It can\'t do that.' });
+          }
+
+          // If we're still valid after this, add the key into the keys array
+          if( invalid === false ) {
             keys.push( v );
           }
       });
 
+      // Loops through the stored keys and checks for any deleted keys
+      // stored_key[] - keys[] == any deleted keys
       if( stored_keys !== null ) {
         i = stored_keys.length;
         while(i--){
           if( ! ~keys.indexOf( stored_keys[i] ) ) {
             deleted_keys.push( stored_keys[i] );
-            $('textarea.'+ stored_keys[i].replace(/[\{,\}]/g, "") ).remove();
-
+            // Remove the label and text area within #edit_regions
+            $('#edit_regions .'+ stored_keys[i].replace( clean_key, "") ).remove();
           }
         }
       }
 
+      // Loop through the valid keys and add labels and textareas for them
       $.each( keys, function( i, v ) {
-        var clear_v = v.replace(/[\{,\}]/g, ""),
-            form_obj = {
-              'name': 'page[edit_regions][' + clear_v + ']',
-              'class': clear_v,
-              'value': existing_keys[clear_v] || 'Default Text for '+v
-            };
+        var new_key = v.replace( clean_key, ""),
 
-        if( $('textarea').hasClass( clear_v )) {
+            textarea = $("<textarea>", {
+              'name': 'page[edit_regions][' + new_key + ']',
+              'class': new_key,
+              'value': existing_keys[new_key] || 'Default Text for '+v
+            }),
+
+            label = $("<label>", {
+              'for': 'page[edit_regions][' + new_key + ']',
+              'text': new_key.replace('_', " "),
+              'class': new_key
+            });
+
+        if( $('textarea').hasClass( new_key )) {
           return;
         }
 
-        $("<textarea>", form_obj ).appendTo('#page_new');
-
+        // Add our textarea to the end of #edit_regions, and inset it's label before it
+        textarea.appendTo('#edit_regions').before(label);
       });
 
+      // Make any old errors go away
       $('#custom_field_errors').empty();
 
+      // Add error messages for an invalid keys
       $.each( invalid_keys, function( i, v ) {
-        if( v !== "{{title}}" ) {
-          $('#custom_field_errors').append("Your key "+ v +" just got regulated, try using another key.</br>");
+        if( v.key !== "{{title}}" ) {
+          $('#custom_field_errors').append(v.msg+"</br>");
         }
       });
 
+      // Globally persist our keys for the next pass
       stored_keys = keys;
+
+      // Set a 1 second timer
       timer = setTimeout( longPoll , 1e3);
 
       if( $.isFunction( callback ) ){
@@ -84,12 +124,15 @@
 
     init: function(){
       var buffer;
+
+      // Start the party with checking the form's inital value
       Regulate.longPoll();
 
+      // Forces the longPoll to run one more time before you submit the form
       $('form').bind('submit', function( e ){
-        var f = e.target;
-
         e.preventDefault();
+
+        var f = e.target;
 
         clearTimeout( timer );
 
@@ -99,13 +142,18 @@
 
       });
 
+      // Runs the longPoll if `}}` is entered into the form
       $(window).bind('keyup', function(e) {
         if( e.keyCode == 221 && buffer == 221 ) {
           Regulate.longPoll( true );
         }
 
         buffer = e.keyCode;
+      });
 
+      // Prevents the longPoll from running if you're focused on the textarea
+      $('#page_view').bind('focus blur', function(e){
+        $(this).toggleClass('active');
       });
     }
 
